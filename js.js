06a3f05007deb2abeb5cee12014f501920ce9704
Ishitv-gigabyte <98424507+Ishitv-gigabyte@users.noreply.github.com>
2023-09-26 21:00:03 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function alert1() {
+    alert("⚠️⚠️You are not allowed to hate me😇😇")
+}
+
+function alert2() {
+    alert("⚠️⚠️Theek hai response text karo aur gussa mat hona, I had to do this😤😤")
+}
